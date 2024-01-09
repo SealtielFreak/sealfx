07db159e62c873ff16deece9f871fdbee1ec50b2
SealtielFreak <Die.Seal_Valderrama@live.com>
2024-01-09 12:24:45 -0600

    Minor changes

--- include/umath.h
+++ include/umath.h
@@ -5,4 +5,7 @@
 #include <stdint.h>
 
 uint16_t mapping_u16(uint16_t x, uint16_t in_min, uint16_t in_max, uint16_t out_min, uint16_t out_max);
+
+uint16_t range_random_u16(uint16_t min, uint16_t max);
+
 #endif //SOUNDPI_PFX_UMATH_H
