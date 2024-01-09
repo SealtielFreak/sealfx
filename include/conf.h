b4bc1f051668cc6efefec11afb8796bf4f97f6e0
SealtielFreak <Die.Seal_Valderrama@live.com>
2024-01-08 22:17:19 -0600

    Improvent audio PCM

--- include/conf.h
+++ include/conf.h
@@ -2,20 +2,20 @@
 #define SOUNDPI_PFX_CONF_H
 
 
-#define get_clkdiv_hz(n) ((48000000.f * 256) / (n * 65536))
+#define get_clkdiv_hz(n) ((48000000.d * 256) / (n * 65536))
 
-#define DEFAULT_FRQ_CPU_KHZ         270000
+#define DEFAULT_FRQ_CPU_KHZ         300000
 
 #define LED_PIN_BUILT               25
 
 #define DEFAULT_GPIO_PWM0           17
 #define DEFAULT_GPIO_PWM1           16
 
-#define DEFAULT_HZ_PWM              1000000
-#define DEFAULT_WRAP_PWM            200
-#define DEFAULT_CLKDIV_PWM          1
+#define DEFAULT_HZ_PWM              48000000 * 4
+#define DEFAULT_WRAP_PWM            64
+#define DEFAULT_CLKDIV_PWM          get_clkdiv_hz(DEFAULT_HZ_PWM)
 
-#define DEFAULT_CLKDIV_ADC          2.0
+#define DEFAULT_CLKDIV_ADC          0
 #define DEFAULT_GPIO_ADC            26
 #define DEFAULT_CHANNEL_ADC         0
 
--- src/interface/audio.c
+++ src/interface/audio.c
@@ -34,8 +34,8 @@ uint16_t read_audio(void) {
 }
 
 void write_audio(uint16_t signal) {
-    const uint16_t ch0 = mapping_u16(signal, 0, 4096, 0, DEFAULT_WRAP_PWM);
+    const uint16_t ch0 = signal;
 
-    pwm_set_chan_level(slice_num_0, chan_num_0, ch0);
-    pwm_set_chan_level(slice_num_1, chan_num_1, ch0 >> 3);
+    pwm_set_chan_level(slice_num_0, chan_num_0, ch0 & 63);
+    pwm_set_chan_level(slice_num_1, chan_num_1, ch0 >> 6);
 }
