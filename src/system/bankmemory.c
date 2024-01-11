dfb06e9ad85faff098f533812ac8ea645e9b93b4
SealtielFreak <43727474+SealtielFreak@users.noreply.github.com>
2024-01-11 13:32:19 -0600

    Minor changes

--- src/system/bankmemory.c
+++ src/system/bankmemory.c
@@ -1,5 +1,7 @@
 #include "system/bankmemory.h"
 
+#include <stdlib.h>
+
 volatile uint16_t global_bank_memory[MAX_BUFFER_GLOBAL_SPACE] = {0};
 
 uint16_t get_counter_value_memory_bank(unsigned long index) {
@@ -16,4 +18,8 @@ void set_counter_value_memory_bank(unsigned long index, uint16_t value) {
     }
 
     global_bank_memory[index] = value;
+}
+
+void flush_memory_bank(void) {
+    memset(global_bank_memory, 0, MAX_BUFFER_GLOBAL_SPACE);
 }
