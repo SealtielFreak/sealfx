093f61f60b074d8b63fc2fdd693224744206aa62
SealtielFreak <43727474+SealtielFreak@users.noreply.github.com>
2024-01-11 13:31:31 -0600

    Minor changes

--- src/interface/audio.c
+++ src/interface/audio.c
@@ -12,16 +12,16 @@
 #include "conf.h"
 
 static uint8_t chan_num_0, slice_num_0, chan_num_1, slice_num_1;
-uint16_t adj_min = 512, adj_max = 4096;
+uint16_t adj_min = 900, adj_max = 4096;
 
 void encode_init(void) {
     chan_num_0 = pwm_gpio_to_channel(DEFAULT_GPIO_PWM0);
     slice_num_0 = pwm_gpio_to_slice_num(DEFAULT_GPIO_PWM0);
-    pwm_init_pin(DEFAULT_GPIO_PWM0, slice_num_0, chan_num_0, get_clkdiv_hz(DEFAULT_HZ_PWM), DEFAULT_WRAP_PWM);
+    pwm_init_pin(DEFAULT_GPIO_PWM0, slice_num_0, chan_num_0, DEFAULT_CLKDIV_PWM, DEFAULT_WRAP_PWM);
 
     chan_num_1 = pwm_gpio_to_channel(DEFAULT_GPIO_PWM1);
     slice_num_1 = pwm_gpio_to_slice_num(DEFAULT_GPIO_PWM1);
-    pwm_init_pin(DEFAULT_GPIO_PWM1, chan_num_1, slice_num_1, get_clkdiv_hz(DEFAULT_HZ_PWM), DEFAULT_WRAP_PWM);
+    pwm_init_pin(DEFAULT_GPIO_PWM1, chan_num_1, slice_num_1, DEFAULT_CLKDIV_PWM, DEFAULT_WRAP_PWM);
 }
 
 void decode_init(void) {
@@ -34,19 +34,15 @@ uint16_t read_audio(void) {
 }
 
 void write_audio(uint16_t signal) {
-    const uint16_t ch0 = cleaner_audio(signal);
+    const uint16_t signal_out = cleaner_audio(signal);
 
-    pwm_set_chan_level(slice_num_0, chan_num_0, ch0 & 63);
-    pwm_set_chan_level(slice_num_1, chan_num_1, ch0 >> 6);
+    pwm_set_chan_level(chan_num_1, chan_num_1, signal_out);
+
+    // pwm_set_chan_level(slice_num_0, chan_num_0, signal_out & 63);
+    // pwm_set_chan_level(slice_num_1, chan_num_1, signal_out >> 6);
 }
 
 uint16_t cleaner_audio(uint16_t signal) {
-    if(signal < adj_min) {
-        return 0;
-    } else if(signal > adj_max) {
-        return 4096;
-    }
-
     return signal;
 }
 
